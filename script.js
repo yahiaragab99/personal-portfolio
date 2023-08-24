4d9ac439df540209b6791fbde9b60a073a0fa2ea
yahiaragab99 <ragabyahia@yahoo.com>
2023-08-24 11:04:44 +0400

    Portfolio-V1

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  const menu =
+    document.querySelector(
+      ".menu-links"
+    )
+  const icon =
+    document.querySelector(
+      ".hamburger-icon"
+    )
+  menu.classList.toggle(
+    "open"
+  )
+  icon.classList.toggle(
+    "open"
+  )
+}
